14a8865a3b4de17d5aceb1b841b69d468cab9d1b
Amos Bafuna <amosbafuna24@gmail.com>
2023-03-09 17:23:29 +0100

    Feat: Add of react spinners

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { Suspense } from "react";
-
-import ErrorBoundary from "./ErrorBoundary";
-import DoesAsynchronousThings from "./DoesAsynchronousThings";
+import { Suspense } from 'react';
+import ScaleLoader from 'react-spinners/ScaleLoader';
+import ErrorBoundary from './ErrorBoundary';
+import DoesAsynchronousThings from './DoesAsynchronousThings';
 
 // The root of the app. It has an Error Boundary, a Suspense Boundary, and a
 // component that demonstrates asynchronous loading.
 export default function App() {
   return (
     <ErrorBoundary>
-      <Suspense fallback="Loading...">
+      <Suspense fallback={<ScaleLoader color="#ff0" />}>
         <DoesAsynchronousThings />
       </Suspense>
     </ErrorBoundary>
